d6ee02135b5e989aad56cfb137054a7fa5f44dfe
xijiangtao <xijiangt@qq.com>
2017-09-08 17:39:27 +0800

    0908

--- project/hex_tools.py
+++ project/hex_tools.py
@@ -1,20 +1,32 @@
 # coding=utf-8
 '''写一些常用的函数'''
 import binascii
-
+from datetime import datetime
+import re
 
 def charArray_hex(array, base=16):
-    '''把从大华光盘映像里的文件读出数值来'''
+    '''把从大华光盘映像里读出来的字符串转换为正确的16进制'''
     array.reverse()
     tmp = '0x'
     for i in array:
         tmp += i[2:]
-    print(tmp)
     tmp = int(tmp, base=base)
     return hex(tmp)
 
 
+'''返回当前时间的16进制，用于筛选时段'''
+def now():
+	now_time = datetime.now()
+	c = str(now_time)[2:-7]
+	result = ''
+	for i in c:
+		t = ord(i)
+		if t > 47 and t < 58:
+			result += i
+	return hex(int(result))
+
+print(now())
 '''
-text1 = text.encode('ascii')       转换为ascii字节码
+text1 = text.encode('ascii')       字符串转换为ascii字节码
 binascii.b2a_hex(text1)            以16进制形式表示
 '''
--- project/test.py
+++ project/test.py
@@ -2,51 +2,28 @@
 
 import time
 import sys
+import asyncio
 
 from hex_tools import charArray_hex
 '''
-解释如下：
-0x00：4字节，数据帧的开始标志（大写的“DHAV”）。
-0x04：2字节，帧类型（0xFD00表示关键帧，0xFC00表示非关键帧）。
-0x06：2字节，通道号（从0开始计数）。
-0x08：4字节，帧编号。
-0x0C：4字节，帧大小，单位字节。
-0x10：4字节，帧时间（Dtime）。
+大华和海康的要求：
+    根据帧编号进行排序
+    提取 8.10 到 当前时间每天 7：00~8：30     17：00~18：30
 '''
 
-
-def sort_frame(file):
+#把处理程序改成异步。
+def process(file):
     '''按照帧的编号排序帧'''
     lists = []
-    f = [[0x48], [0x41], [0x56]]
-    with open(file, 'rb') as f:
-        while True:
-            b = f.read(1)  
-            if b == bytes([0x44]):
-                b = f.read(1)
-                if b == bytes([0x48]):
-                    b = f.read(1)
-                    if b == bytes([0x41]):
-                        b = f.read(1)
-                        if b == bytes([0x56]):
-                            print('捕获成功')
-                            print('实际读:', f.read(20))
-                            bs = f.read(16)
-                            print('bs:', bs)
-                            lists.append(create_dict(bs))
-    lists = sorted(lists, key=cmp)
-    print(lists)
     return lists
 
+def sort_frame(lists):    #外部归并排序
+    pass
 
-def cmp(frame):
-    return frame['number']
 
-
-def create_dict(b):  # 这个通篇都是捉急，啊啊啊啊
+def create_dict(b):  # 通篇都是捉急
     frame = {}
     bytess = [hex(x) for x in bytes(b)]
-    print('bytess:', bytess)
     frame['number'] = charArray_hex(bytess[4:8])
     frame['size'] = charArray_hex(bytess[8:12])
     frame['datetime'] = charArray_hex(bytess[-4:])
@@ -61,16 +38,12 @@ def write_file(lists):
             for key in i:
                 f.write(key + ':')
                 f.write(''.join(i[key]) + '\t')
-        f.write('\n')
+            f.write('\n')
 
 
-'''文件位置：E:\dahua:TEST1'''
+
 if __name__ == "__main__":
-    start = time.time()
-    print('开始')
     file = sys.argv[1]
-    lists = sort_frame(file)
-    write_file(lists)
-
-    end = time.time()
-    print('这尼玛跑到多会了，操：', end - start)
+    while True:
+        lists = process(file)
+        write_file(lists)
