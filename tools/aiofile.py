97ef3d36a44a3ea87b2518864ea665431edf30d5
xijiangtao <xijiangt@qq.com>
2017-09-12 16:36:25 +0800

    tools

---
+++ tools/aiofile.py
@@ -0,0 +1,15 @@
+#! /usr/bin/dev python3 
+#coding=utf8
+
+
+#py3.5版本的异步读写
+import asyncio
+import threading
+
+
+#实现with打开，需要 __enter__,__exit__方法
+class AsnycFile():
+	def __init__():
+		pass
+
+	
